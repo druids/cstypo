6fb761232e97701b63fdd18298c10c5205c75b8a
matllubos <matllubos@gmail.com>
2018-11-05 14:33:06 +0100

    0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='cstypo',
-    version='0.1.4',
+    version='0.1.5',
     author='Juda Kaleta',
     author_email='juda.kaleta@gmail.com',
     packages=['cstypo', 'cstypo.templatetags', 'cstypo.tests'],
